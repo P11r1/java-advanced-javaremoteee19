93f37edd1ea298097be35f8c470a51f2eb7ce828
Marko <Piirmarko92@gmail.com>
2022-10-27 21:02:15 +0300

    Exception examples added

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -6,6 +6,8 @@ import org.sda.services.implementations.PersonServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
+
+
         //INTERFACE
         Person person = new Person();
         person.setFirstName("Marko");
@@ -21,6 +23,22 @@ public class Main {
 
         System.out.println("Person's last name: " + person.getLastName());
 
+        //EXCEPTION
+        //Try/catch exception, program can run forward and prints "sout" instead of exception
+        //Exception handling
+        try{
+            int[] intArray = {1, 2, 5, 6};
 
+            for (int i = 0; i <= intArray.length; i++) {
+                System.out.println(intArray[i]);
+            }
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("The number cannot be printed because it's unavailable in the array");
+        } catch (Exception  exception) {
+            System.out.println("Exception being caught");
+        } finally { // This block will be executed irrespective of catch blocks
+            int a = 15;
+            System.out.println("Finally executed, a: " + a);
+        }
     }
 }
