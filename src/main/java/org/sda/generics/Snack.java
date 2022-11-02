61af7332bff17b978a0a0302c0bcd389ca136080
Marko <Piirmarko92@gmail.com>
2022-11-02 20:23:40 +0200

    List examples added

---
+++ src/main/java/org/sda/generics/Snack.java
@@ -0,0 +1,19 @@
+package org.sda.generics;
+
+/**
+ * Implementation of Comparable interface
+ *
+ * @author Marko
+ */
+public class Snack implements Comparable<Snack> {
+    private int tasteRating;
+
+    public Snack(int tasteRating) {
+        this.tasteRating = tasteRating;
+    }
+
+    @Override
+    public boolean isBetter(Snack item) {
+        return this.tasteRating > item.tasteRating;
+    }
+}
